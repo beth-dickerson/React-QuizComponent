55f2e949494aa56e97fd59504e77d4f8f4244d12
Beth Dickerson <bethdickerson17@gmail.com>
2018-06-01 15:49:22 -0400

    render div with className in Quiz component

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,7 +10,9 @@ class Quiz extends Component {
     render() {
         return (
             <div>
-                Quiz
+                <div className="QuizQuestions">
+                    {quizData.quiz_questions[0].instruction_text}
+                </div>
             </div>
         )       
     }
