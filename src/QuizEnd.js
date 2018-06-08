ccb8f08cf81a984b5701375f2302954fb5fed346
Beth Dickerson <bethdickerson17@gmail.com>
2018-06-08 16:40:38 -0400

    created QuizEnd.js, I think I wrote my const wrong in Quiz.js

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 
 class QuizEnd extends Component {
-    state = {  }
     render() {
         return (
             <div>
                 <p>Thanks for playing!</p>
-                <a href=" ">Reset Quiz</a>
+                <a href=''>Reset Quiz</a>
             </div>            
         )
     }
